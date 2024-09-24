051d1b16a408851e131af1796ac7aee0630b7b51
Bobby Roe <bobby.roe@unifyconsulting.com>
2024-09-24 07:17:11 -0700

    add mouse interactivity

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import getStarfield from "./src/getStarfield.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(45, innerWidth / innerHeight, 0.1, 1000);
-camera.position.set(0, 0, 3.5);
+camera.position.set(0, 0, 4);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(innerWidth, innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -13,22 +13,29 @@ document.body.appendChild(renderer.domElement);
 const orbitCtrl = new OrbitControls(camera, renderer.domElement);
 orbitCtrl.enableDamping = true;
 
+const raycaster = new THREE.Raycaster();
+const pointerPos = new THREE.Vector2();
+const globeUV = new THREE.Vector2();
+
 const textureLoader = new THREE.TextureLoader();
 const starSprite = textureLoader.load("./src/circle.png");
-const colorMap = textureLoader.load("./src/04_rainbow1k.jpg");
+const otherMap = textureLoader.load("./src/04_rainbow1k.jpg");
+const colorMap = textureLoader.load("./src/00_earthmap1k.jpg");
 const elevMap = textureLoader.load("./src/01_earthbump1k.jpg");
 const alphaMap = textureLoader.load("./src/02_earthspec1k.jpg");
 
 const globeGroup = new THREE.Group();
 scene.add(globeGroup);
 
-const geo = new THREE.IcosahedronGeometry(1, 10);
+const geo = new THREE.IcosahedronGeometry(1, 16);
 const mat = new THREE.MeshBasicMaterial({ 
-  color: 0x202020,
+  color: 0x0099ff,
   wireframe: true,
+  transparent: true,
+  opacity: 0.1
  });
-const cube = new THREE.Mesh(geo, mat);
-globeGroup.add(cube);
+const globe = new THREE.Mesh(geo, mat);
+globeGroup.add(globe);
 
 const detail = 120;
 const pointsGeo = new THREE.IcosahedronGeometry(1, detail);
@@ -36,9 +43,11 @@ const pointsGeo = new THREE.IcosahedronGeometry(1, detail);
 const vertexShader = `
   uniform float size;
   uniform sampler2D elevTexture;
+  uniform vec2 mouseUV;
 
   varying vec2 vUv;
   varying float vVisible;
+  varying float vDist;
 
   void main() {
     vUv = uv;
@@ -47,6 +56,16 @@ const vertexShader = `
     vec3 vNormal = normalMatrix * normal;
     vVisible = step(0.0, dot( -normalize(mvPosition.xyz), normalize(vNormal)));
     mvPosition.z += 0.35 * elv;
+
+    float dist = distance(mouseUV, vUv);
+    float zDisp = 0.0;
+    float thresh = 0.04;
+    if (dist < thresh) {
+      zDisp = (thresh - dist) * 10.0;
+    }
+    vDist = dist;
+    mvPosition.z += zDisp;
+
     gl_PointSize = size;
     gl_Position = projectionMatrix * mvPosition;
   }
@@ -54,22 +73,31 @@ const vertexShader = `
 const fragmentShader = `
   uniform sampler2D colorTexture;
   uniform sampler2D alphaTexture;
+  uniform sampler2D otherTexture;
 
   varying vec2 vUv;
   varying float vVisible;
+  varying float vDist;
 
   void main() {
     if (floor(vVisible + 0.1) == 0.0) discard;
     float alpha = 1.0 - texture2D(alphaTexture, vUv).r;
     vec3 color = texture2D(colorTexture, vUv).rgb;
+    vec3 other = texture2D(otherTexture, vUv).rgb;
+    float thresh = 0.04;
+    if (vDist < thresh) {
+      color = mix(color, other, (thresh - vDist) * 50.0);
+    }
     gl_FragColor = vec4(color, alpha);
   }
 `;
 const uniforms = {
   size: { type: "f", value: 4.0 },
   colorTexture: { type: "t", value: colorMap },
+  otherTexture: { type: "t", value: otherMap },
   elevTexture: { type: "t", value: elevMap },
-  alphaTexture: { type: "t", value: alphaMap }
+  alphaTexture: { type: "t", value: alphaMap },
+  mouseUV: { type: "v2", value: new THREE.Vector2(0.0, 0.0) },
 };
 const pointsMat = new THREE.ShaderMaterial({
   uniforms: uniforms,
@@ -87,14 +115,30 @@ scene.add(hemiLight);
 const stars = getStarfield({ numStars:4500, sprite: starSprite });
 scene.add(stars);
 
+function handleRaycast() {
+  raycaster.setFromCamera(pointerPos, camera);
+  const intersects = raycaster.intersectObjects([globe], false);
+  if (intersects.length > 0) {
+    globeUV.copy(intersects[0].uv);
+  }
+  uniforms.mouseUV.value = globeUV;
+}
+
 function animate() {
   renderer.render(scene, camera);
   globeGroup.rotation.y += 0.002;
-
+  handleRaycast();
   requestAnimationFrame(animate);
   orbitCtrl.update();
 };
 animate();
+
+window.addEventListener('mousemove', (evt) => {
+  pointerPos.set(
+    (evt.clientX / window.innerWidth) * 2 - 1,
+    -(evt.clientY / window.innerHeight) * 2 + 1
+  );
+});
 
 window.addEventListener('resize', function () {
   camera.aspect = window.innerWidth / window.innerHeight;
